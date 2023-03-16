8d9fd237dd878ebeb6295ead7a278c5a5db260b2
ngruenwald <40895194+ngruenwald@users.noreply.github.com>
2023-03-16 16:44:16 +0100

    feat: create proper module
    * move everything into 'cgen' folder
    * use subfolder for 'templates'
    * bump version to 0.2.0

---
+++ cgen/__main__.py
@@ -0,0 +1,13 @@
+import logging
+import sys
+
+from .cgen import cgen
+
+
+if __name__ == '__main__':
+    try:
+        result = cgen()
+    except Exception as error:
+        logging.fatal(error)
+        result = 1
+    sys.exit(result)
--- cgen/spec_types.py
+++ cgen/spec_types.py
@@ -1,5 +1,6 @@
 from typing import List, Dict, Optional
-from doc import DocEntry
+
+from .doc import DocEntry
 
 
 class Type:
