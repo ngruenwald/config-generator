4d76ca261dab457e81b905fc4a4b75edf3e6e2ba
ngruenwald <40895194+ngruenwald@users.noreply.github.com>
2024-12-15 20:13:43 +0100

    feat: "PostProcessConfig"
    This introduces config post-processing.
    Two new options for the definition file:
      * options.cpp.post_process.use [bool]
      * options.cpp.post_process.args [list of str]
    When enabled the c++ templates will create a PostProcessConfig function which will take a config object, a callback function, and some optional parameters defined by the definition.
    The post-processing will call the callback function for every available field (empty optionals are currently not processed).

--- tests/cpp-xxxx/main.cpp
+++ tests/cpp-xxxx/main.cpp
@@ -6,6 +6,14 @@
 #define _TEST(cond, err, line) if (cond) { _PRINTLINE(#cond, "ok", line); } else { err += 1; _PRINTLINE(#cond, "!!", line); }
 #define TEST(cond) _TEST(cond, errors, __LINE__)
 
+// helper type for the visitor
+template<class... Ts>
+struct overloaded : Ts... { using Ts::operator()...; };
+
+// explicit deduction guide (not needed as of C++20)
+template<class... Ts>
+overloaded(Ts...) -> overloaded<Ts...>;
+
 
 int main(int argc, char** argv)
 {
@@ -76,6 +84,86 @@ int main(int argc, char** argv)
     TEST(config->test42.base_oid == 0xe001);
 
     std::cout << '\n' << *config << '\n';
+
+
+    auto process_fn =
+        [] (const std::vector<std::string>& key,
+            config::post_process_types_t& value,
+            const std::string& foo) -> void
+        {
+            std::ostringstream oss;
+
+            for (const auto& k : key)
+            {
+                oss << '.' << k;
+            }
+            oss << ": ";
+
+            std::visit(
+                overloaded{
+                    [&oss] (auto arg)
+                    {
+                        //oss << (*arg);
+                    },
+                    [&oss] (config::LittleEnum* arg)
+                    {
+                        oss << to_string(*arg);
+
+                        *arg = config::LittleEnum::Bar;
+                    },
+                    [&oss] (int* arg)
+                    {
+                        oss << *arg;
+                    },
+                    [&oss] (unsigned int* arg)
+                    {
+                        oss << *arg;
+                    },
+                    [&oss] (float* arg)
+                    {
+                        oss << *arg;
+                    },
+                    [&oss] (double* arg)
+                    {
+                        oss << *arg;
+                    },
+                    [&oss] (bool* arg)
+                    {
+                        oss << (*arg ? "true" : "false");
+                    },
+                    [&oss] (std::string* arg)
+                    {
+                        oss << '\"' << *arg << '\"' << '\n';
+                    },
+                    [&oss] (std::vector<std::string>* arg)
+                    {
+                        oss << '[';
+                        for (const auto& e : *arg)
+                        {
+                            oss << '\"' << e << '\"' << ',';
+                        }
+                        oss << ']' << '\n';
+                    },
+                    [&oss] (std::map<std::string, std::string>* arg)
+                    {
+                        oss << '{';
+                        for (const auto& [k,v] : *arg)
+                        {
+                            oss << '\"' << k << '\"' << ':' << '\"' << v << '\"' << ',';
+                        }
+                        oss << '}';
+                    }
+                },
+                value
+            );
+
+            oss << '\n';
+            std::cout << oss.str();
+        };
+
+    config::PostProcessConfig(*config, process_fn, "foo");
+
+    TEST(config->an_enum == config::LittleEnum::Bar);
 
     return errors;
 }
